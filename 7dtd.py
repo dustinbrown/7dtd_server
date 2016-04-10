68a1fc9012a1e4bd9e3b526a15d022ec336eead5
dbrown <dbrown@jamasoftware.com>
2016-04-10 14:47:22 -0700

    first commit

---
+++ 7dtd.py
@@ -0,0 +1,85 @@
+import boto.ec2
+import logging
+import requests
+import click
+import yaml
+
+logging.basicConfig(level=logging.INFO)
+
+class Server(object):
+  def __init__(self):
+    stream = file('config.yaml', 'r')
+    self.cfg = yaml.load(stream) 
+
+    name_tag = '7dtd'
+    filters = {"tag:Name": name_tag}
+
+    try:
+      self.conn = boto.ec2.connect_to_region(
+        "us-west-2",
+        profile_name=self.cfg['profile_name']
+      )
+    except boto.provider.ProfileNotFoundError as e:
+      logging.error("AWS profile not found")
+      self.help()
+      raise SystemExit
+
+    reservations = self.conn.get_all_reservations(filters=filters)
+    try:
+      self.instance_id = reservations[0].instances[0].id
+    except Exception:
+      logging.fatal(
+          "no instance found in AWS EC2 with a name tag: {0}".format(name_tag))
+      raise SystemExit
+
+  def start(self):
+    self.conn.start_instances(instance_ids=[self.instance_id])
+    logging.info("Starting server")
+
+  def stop(self):
+    self.conn.stop_instances(instance_ids=[self.instance_id])
+    logging.info("Stopping server")
+
+  def help(self):
+    pass
+
+  def is_game_running(self):
+    ''' An unfortunate way to check if the game is online
+        Surely there is a better way to do this
+    '''
+    try:
+      r = requests.get('http://{0}:26900'.format(self.cfg['game_server']))
+    except requests.ConnectionError as e:
+      if 'Connection refused' in  e.message[1]:
+        return False
+
+    return True
+
+  def status(self):
+    statuses = self.conn.get_all_instance_status(instance_ids=self.instance_id)
+    if len(statuses) == 1:
+      status = statuses[0]
+      state = status.state_name
+    elif len(statuses) == 0:
+      logging.info("Server is offline")
+      raise SystemExit
+    else:
+      logging.warn("More than 1 instances found, something is wrong")
+      raise SystemExit
+
+    if state == 'running':
+      if not self.is_game_running():
+        logging.info("Server is running but game service is offline")
+      else:
+        logging.info(
+            "EVERYTHING IS OKAY. Server is running and game service is online")
+
+@click.command()
+@click.argument('operation', nargs=1, type=click.Choice(['start', 'stop', 'status']))
+def main(operation):
+  server = Server()
+  op = getattr(server, operation)
+  op()
+
+if __name__ == '__main__':
+      main()
